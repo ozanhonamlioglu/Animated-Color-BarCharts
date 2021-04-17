3eef137de209da5c147f0416c20a62bad2a66488
ozan honamlioglu <ozan.honamlioglu@inveon.com>
2021-04-17 10:21:10 +0300

    Initial Commit

---
+++ AnimatedBarCharts/AnimatedBarChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedBarChartsApp.swift
+//  AnimatedBarCharts
+//
+//  Created by ozy on 17.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedBarChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedBarCharts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatedBarCharts
+//
+//  Created by ozy on 17.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
